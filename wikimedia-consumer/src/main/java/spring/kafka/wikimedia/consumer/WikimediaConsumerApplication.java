1bd9f4df5651f425f46953afd80e02ee3077d245
viraj-vvk <viraj.vvk@gmail.com>
2024-12-23 16:31:00 +0530

    Initial setup with producer and consumer modules

---
+++ wikimedia-consumer/src/main/java/spring/kafka/wikimedia/consumer/WikimediaConsumerApplication.java
@@ -0,0 +1,13 @@
+package spring.kafka.wikimedia.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaConsumerApplication.class, args);
+	}
+
+}
---
+++ wikimedia-producer/src/main/java/spring/kafka/wikimedia/producer/WikimediaProducerApplication.java
@@ -0,0 +1,13 @@
+package spring.kafka.wikimedia.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaProducerApplication.class, args);
+	}
+
+}
